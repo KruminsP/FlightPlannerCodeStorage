82faa16037bff4ce3ee646939e8d172483e81cd8
Peterson <55082100+Astonkajis@users.noreply.github.com>
2022-09-23 11:31:45 +0300

    Add project files.

---
+++ Controllers/AdminApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("admin-api")]
+    [ApiController]
+    public class AdminApiController : ControllerBase
+    {
+        [Route("flights/{id}")]
+        [HttpGet]
+        public IActionResult GetFlight(int id)
+        {
+            return Ok("hello");
+        }
+    }
+}
